41a6c48167744be7e0170b801b664d04389fceaa
Scott Fennie <s.fennie7@gmail.com>
2021-09-12 19:18:45 -0600

    done

--- app/Models/TodoItem.js
+++ app/Models/TodoItem.js
@@ -19,7 +19,7 @@ export class TodoItem {
             <span class="ps-1 text-break d-flex flex-wrap ${this.completed == true ? "text-decoration-line-through" : ""}"
             >${this.description}</span>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi text-danger bi-x select" onclick="app.userController.deleteTodo('${this.id}')" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi text-danger bi-x select selectable" onclick="app.userController.deleteTodo('${this.id}')" viewBox="0 0 16 16">
     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
   </svg>
             </div>
